598f6c1900f224e3495a1199be2e7b31d1056668
The Typhothanian <thetyphothanian@gmail.com>
2025-12-22 14:28:16 -0700

    no more riptide in rain, ancient debris is now more common

--- common/src/main/java/net/typho/beryllium/mixin/tridents/TridentItemMixin.java
+++ common/src/main/java/net/typho/beryllium/mixin/tridents/TridentItemMixin.java
@@ -4,10 +4,12 @@ import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TridentItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +26,16 @@ public class TridentItemMixin {
     )
     private <T> Optional<T> releaseUsing(ItemStack stack, DataComponentType<List<T>> componentType, Operation<Optional<T>> original) {
         return original.call(stack, componentType).or(() -> Optional.of((T) SoundEvents.TRIDENT_RIPTIDE_3));
+    }
+
+    @Redirect(
+            method = "releaseUsing",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Player;isInWaterOrRain()Z"
+            )
+    )
+    private boolean releaseUsing(Player instance) {
+        return instance.isInWater();
     }
 }
