06651209565ff990c83e039fdfbdec385fec6bd2
The Typhothanian <thetyphothanian@gmail.com>
2025-12-23 17:02:50 -0700

    quartered length of shulker bullet levitation

---
+++ common/src/main/java/net/typho/beryllium/mixin/end_city/ShulkerBulletMixin.java
@@ -0,0 +1,17 @@
+package net.typho.beryllium.mixin.end_city;
+
+import net.minecraft.world.entity.projectile.ShulkerBullet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(ShulkerBullet.class)
+public class ShulkerBulletMixin {
+    @ModifyConstant(
+            method = "onHitEntity",
+            constant = @Constant(intValue = 200)
+    )
+    private int onHitEntity(int constant) {
+        return constant / 4;
+    }
+}
